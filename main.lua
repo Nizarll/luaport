a0c9682deac2e2fa141b723356191e419bd39a4a
Nizarll <forsubsofmy@gmail.com>
2024-08-03 23:03:41 +0000

    changes to root page

--- main.lua
+++ main.lua
@@ -1,48 +1,52 @@
-local server = require 'server'
-local router = require 'router'
-local utils = require 'utils'
+local server = require("server")
+local router = require("router")
+local utils = require("utils")
 
-local routes = 'routes/'
-local webserver = server.new('localhost', 8080)
+local routes = "routes/"
+local webserver = server.new("localhost", 8080)
 
 routes = {
-  root = router.new('/',
-    routes .. 'root/',
-    'main.html',
-    router.new(
-      '/css/styles.css',
-      'routes/css/styles.css'
-    ),
-    router.new(
-      '/main.js',
-      'routes/root/main.js'
-    )
-  ),
+	root = router.new(
+		"/",
+		routes .. "root/",
+		"main.html",
+		router.new("/css/styles.css", "routes/css/styles.css"),
+		router.new("/main.js", "routes/root/main.js"),
+		router.new("/profile.png", "routes/root/profile.png")
+	),
 }
 
 function get_path(header)
-  for key, route in pairs(routes) do
-    if utils.ends_with(header, 'css') and header == route.css.header then return route.css.path
-    elseif utils.ends_with(header, 'js') and header == route.js.header then return route.js.path
-    elseif route.header == header then return route.html end
-  end
+	for key, route in pairs(routes) do
+		if utils.ends_with(header, "css") and header == route.css.header then
+			return route.css.path
+		elseif utils.ends_with(header, "js") and header == route.js.header then
+			return route.js.path
+		elseif utils.ends_with(header, "png") and header == route.route1.header then
+			print(route.route1.header .. " " .. route.route1.path)
+			return route.route1.path
+		elseif route.header == header then
+			return route.html
+		end
+	end
 end
 
 while 1 do
-  local request = webserver:await_request()
-  if not request then goto continue end
-  print('h')
-  if request.body == 'favicon.ico' then
-    request:discard()
-  end
-  print('client request : ' .. request.header .. ' : ' .. request.body)
-  local content = utils.read(get_path(request.body) or 'routes/root/main.html')
-  if utils.ends_with(request.body, 'css') then
-    request:reply_with('css', content)
-  elseif utils.ends_with(request.body, 'js') then
-    request:reply_with('js', content)
-  else
-    request:reply_with('html', content)
-  end
-  ::continue::
+	local request = webserver:await_request()
+	if not request then
+		goto continue
+	end
+	if request.body == "favicon.ico" then
+		request:discard()
+	end
+	print("client request : " .. request.header .. " : " .. request.body)
+	local content = utils.read(get_path(request.body) or "routes/root/main.html")
+	if utils.ends_with(request.body, "css") then
+		request:reply_with("css", content)
+	elseif utils.ends_with(request.body, "js") then
+		request:reply_with("js", content)
+	else
+		request:reply_with("html", content)
+	end
+	::continue::
 end
--- router.lua
+++ router.lua
@@ -1,22 +1,30 @@
-local http_server = require  'http.server'
-local http_headers = require 'http.headers'
+local http_server = require("http.server")
+local http_headers = require("http.headers")
 local route = {}
 route.__index = route
 
-function route.new(header, path, html, css, js)
-  local self = setmetatable({
-    header = header,
-    path = path,
-  }, route)
-  if html then
-    self.html = self.path .. html
-  end
-  if css then
-    self.css = css
-  end
-  if js then
-    self.js = js
-  end
-  return self
+function route.new(header, path, html, css, js, ...)
+	local self = setmetatable({
+		header = header,
+		path = path,
+	}, route)
+	if html then
+		self.html = self.path .. html
+	end
+	if css then
+		self.css = css
+	end
+	if js then
+		self.js = js
+	end
+	local args = { ... }
+	if #args <= 0 then
+		return self
+	end
+	for i, arg in pairs(args) do
+		print("route" .. i)
+		self["route" .. i] = arg
+	end
+	return self
 end
 return route
